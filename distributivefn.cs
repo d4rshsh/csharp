60220d96ea8f7f71fa8c67e06ca16ea21c4a93ed
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-21 12:36:53 +0530

    Update distributivefn.cs

--- distributivefn.cs
+++ distributivefn.cs
@@ -4,11 +4,16 @@ namespace aug17product
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Problem6 - Input Age");
-            int age;
-            Console.WriteLine("Enter your age");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("You look older than {0}", age);
+            Console.WriteLine("Problem 5 ");
+            int a, b, c;
+            Console.WriteLine("Enter three numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            int op1 = (a + b) * c;
+            int op2 = (a * b) + (b * c);
+            Console.WriteLine("(x+y).z={0}", op1);
+            Console.WriteLine("(x.y)+(y.z)={0}", op2);
             Console.ReadLine();
         }
     }
