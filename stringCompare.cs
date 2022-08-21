907f18d2dd5ac1d49c76e7d67d1407f63741ad10
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-21 15:02:41 +0530

    20th

---
+++ stringCompare.cs
@@ -0,0 +1,8 @@
+string s1, s2;
+Console.WriteLine("Enter the strings");
+s1 = Console.ReadLine();
+s2 = Console.ReadLine(); 
+bool result;
+result = s1.Equals(s2);
+Console.WriteLine(result); // returns boolean
+Console.ReadLine();
