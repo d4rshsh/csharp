389c7521df60d99ff8ad0a287349496f3081a0e4
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-21 15:01:43 +0530

    19th

---
+++ compareChar.cs
@@ -0,0 +1,15 @@
+int i, j;
+Console.WriteLine("Enter the characters");
+i = Convert.ToChar(Console.ReadLine());
+j = Convert.ToChar(Console.ReadLine());
+if (i == j)
+{
+    Console.WriteLine("Characters are same");
+
+}
+else
+{
+    Console.WriteLine("Characters are not same");
+
+}
+Console.ReadLine();
