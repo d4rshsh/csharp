90e8b38b2e95295eb5e2243898550ae3782c9119
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-21 13:33:36 +0530

    16th problem

---
+++ IncrementalAssignment.cs
@@ -0,0 +1,34 @@
+namespace IncrementalAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string first_name, last_name;
+            Console.WriteLine("Enter your first name");
+            first_name = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            last_name = Console.ReadLine();
+            string user_name, pwd;
+            Console.WriteLine("Enter uname");
+            user_name = Console.ReadLine();
+            Console.WriteLine("Password");
+            pwd = Console.ReadLine();
+            if ((first_name == user_name) && (last_name == pwd) && (!String.IsNullOrEmpty(user_name)) && (!String.IsNullOrEmpty(pwd)))
+                Console.WriteLine("Welcome to my app" + " " + first_name + " " + last_name);
+            else
+            {
+
+                if (first_name != user_name && last_name != pwd)
+                    Console.WriteLine("Invalid Credentials");
+                else if (first_name != user_name)
+                    Console.WriteLine("uname unmatched");
+                else if ((String.IsNullOrEmpty(user_name)) || (String.IsNullOrEmpty(pwd)))
+                    Console.WriteLine("Username or password can't be null");
+                else
+                    Console.WriteLine("Invalid Password");
+
+            }
+            Console.ReadLine();
+        }
+    }
