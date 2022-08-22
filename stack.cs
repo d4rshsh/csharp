69a8fc61fd19f2e615b7de360941bf308c1e4dcf
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-22 22:36:11 +0530

    6th

---
+++ stack.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+
+    internal class Prob6
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            Console.WriteLine("Pushing into stack");
+            for (int i = 0; i < 4; i++)
+            {
+                s.Push(i); // push inbuilt
+            }
+            object a = s.Pop(); // pop from stack
+            Console.WriteLine("Popped Element {0} ", a);
+            int count = s.Count;
+            Console.WriteLine("Count {0} ", count);
+            Console.WriteLine("Top element {0}", s.Peek()); // top element of stack
+
+
+
+            Console.ReadLine();
+
+        }
+    }
