56200fcc458d01f137f931de32d8bb404d1b0fcf
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-23 00:04:53 +0530

    12th

---
+++ minmaxArray.cs
@@ -0,0 +1,32 @@
+/*
+C# prg to Find the minimum and maximum element in an array
+ */
+internal class Prob12
+{
+    static void Main(string[] args)
+    {
+
+        int n;
+        Console.WriteLine("Enter size of array");
+        n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        Console.WriteLine("Enter the numbers");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+                max = arr[i];
+            if (arr[i] < min)
+                min = arr[i];
+        }
+        Console.WriteLine("Maximum element is {0} \n Minimum element is {1}", max, min);
+        Console.ReadLine();
+
+    }
+}
+
