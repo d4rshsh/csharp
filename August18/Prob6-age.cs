8b64643382a78833b1178f3b94643948b4d7b56d
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-23 10:13:22 +0530

    6th

---
+++ August18/Prob6-age.cs
@@ -0,0 +1,14 @@
+namespace aug18
+{
+    internal class Prob6
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You look older than {0}", age);
+            Console.ReadLine();
+        }
+    }
+}
