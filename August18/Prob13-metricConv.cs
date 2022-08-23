6af6c8b25053d99cecb2336c850bf684f83dc919
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-23 10:36:25 +0530

    13th

---
+++ August18/Prob13-metricConv.cs
@@ -0,0 +1,22 @@
+namespace aug18
+{
+    internal class Prob13
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Problem 13 - metre to kilometre conversion");
+            int m;
+            Console.WriteLine("Enter metres");
+            m = Convert.ToInt32(Console.ReadLine());
+            float km = m / 1000;
+            Console.WriteLine("meter to km is {0}", km);
+            int kilo;
+            Console.WriteLine("Enter kilometer");
+            kilo = Convert.ToInt32(Console.ReadLine());
+            int metre;
+            metre = 1000 * kilo;
+            Console.WriteLine("km to meter is {0}", metre);
+            Console.ReadLine();
+        }
+    }
+}
