ce0858a88dd87ef48d1b1bec71304142840d005e
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-23 10:28:41 +0530

    9th,14th

---
+++ August18/Prob9,14-tempConv.cs
@@ -0,0 +1,18 @@
+namespace aug18tempconv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Problem 9 and 14 - temperature conversion");
+            float celsius;
+            Console.WriteLine("Enter celsium to convert K,F");
+            celsius = Convert.ToInt32(Console.ReadLine());
+            float kelvin = 273 + celsius;
+            Console.WriteLine("Kelvin value is {0} degree K", kelvin);
+            float fahren = ((celsius * 9) / 5) + 32;
+            Console.WriteLine("Fahrenheit value is {0} degree F", fahren);
+            Console.ReadLine();
+        }
+    }
+}
