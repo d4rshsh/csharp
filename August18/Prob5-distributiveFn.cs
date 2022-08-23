fdf7cb4e43e2ca517135082248b8437c0a62838e
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-23 10:11:46 +0530

    5th

---
+++ August18/Prob5-distributiveFn.cs
@@ -0,0 +1,20 @@
+namespace aug18
+{
+    internal class Prob5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problem 5 ");
+            int a, b, c;
+            Console.WriteLine("Enter three numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            int op1 = (a + b) * c;
+            int op2 = (a * b) + (b * c);
+            Console.WriteLine("(x+y).z={0}", op1);
+            Console.WriteLine("(x.y)+(y.z)={0}", op2);
+            Console.ReadLine();
+        }
+    }
+}
