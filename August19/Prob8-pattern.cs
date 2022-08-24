f8ea76f9ae458a3b3fac6e1a6b173c6a5d63f8b2
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-24 15:29:56 +0530

    8th

---
+++ August19/Prob8-pattern.cs
@@ -0,0 +1,25 @@
+/* Program to display given pattern
+*
+**
+***
+****
+*****
+ */
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n;
+        Console.WriteLine("Enter the number");
+        n = Convert.ToInt32(Console.ReadLine());
+        int i, j;
+        for (i = 1; i <= n; i++)
+        {
+            for (j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.Write("\n");
+        }
+    }
+}
