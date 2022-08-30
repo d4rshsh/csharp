758a49899c44776673536824e37fd4e1dbdf25a1
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-08-30 14:19:57 +0530

    Program.cs

--- Aug29/Program.cs
+++ Aug29/Program.cs
@@ -40,6 +40,41 @@ namespace Aug29
 
             Console.WriteLine("Items are : ");
             Q.Queue(); */
+            // LinkedList
+            // string linked List
+            
+           /* GenericsLinkedList<string> stringLinkedList = new GenericsLinkedList<string>(); //object 1
+            string s1 = "1. Lionel Messi";
+            string s2 = "2. Pedri Gonzalez";
+            string s3 = "3. Erling Braut Haaland";
+            string s4 = "4. Kevin De Bruyne";
+            stringLinkedList.AddHead(s1);
+            stringLinkedList.AddHead(s2);
+            stringLinkedList.AddHead(s3);
+            stringLinkedList.AddHead(s4);
+            //display List
+            foreach (string str in stringLinkedList)
+            {
+                Console.WriteLine(str);
+            }
+
+            //Integer LinkedList
+            GenericsLinkedList<int> integerList = new GenericsLinkedList<int>();
+            int n1 = 1;
+            int n2 = 2;
+            int n3 = 3;
+
+            integerList.AddHead(n1);
+            integerList.AddHead(n2);
+            integerList.AddHead(n3);
+
+            foreach (int Intger in integerList)
+            {
+                Console.WriteLine(Intger);
+            }
+
+
+            Console.ReadKey(); */
             Console.ReadLine();
         }
     }
