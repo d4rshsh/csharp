1161b5e5b731b06daf8fa084e9e00b234bf2e02e
darshshsh <ds14@gmail.com>
2022-09-07 16:19:31 +0530

    Sept06

---
+++ Sept06/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public abstract class Person
+    {
+        public abstract void work();
+
+    }
+    public class employees : Person
+    {
+
+
+        public override void work()
+        {
+            Console.WriteLine("calling emplpoyee");
+        }
+    }
+    public class manager : Person
+    {
+
+
+        public override void work()
+        {
+            Console.WriteLine("calling manager");
+        }
+    }
+    public class clerk : Person
+    {
+
+
+        public override void work()
+        {
+            Console.WriteLine("calling clerk");
+        }
+    }
+}
+
