5dd5fe3988e1504a79c1a1c2f2b95dff8d6ebed1
darshshsh <ds14@gmail.com>
2022-09-08 16:51:19 +0530

    Sept07

---
+++ Sept07/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sep07
+{
+    public delegate void stren(string s);
+    public delegate void stren2(int i, int j);
+    public delegate void stren3(Products p);
+    public delegate int stren4(DateTime t);
+    public class strin
+    {
+        //  PS 1
+        public void stringlen(string s)
+        {
+            Console.WriteLine("Length of string entered is {0} ",s.Length);
+
+        }
+        // PS 2
+        public void greet(string s)
+        {
+            Console.WriteLine($"Hello {s}, how are you doing today?");
+        }
+    }
+
+        // PS 3
+    public class integer
+    {
+        public void Add(int i, int j)
+        {
+            Console.WriteLine($"Addition = {i + j}");
+        }
+        public void Sub(int i, int j)
+        {
+            if (i > j)
+            {
+                Console.WriteLine($"Subtraction = {i - j}");
+            }
+            else
+            {
+                Console.WriteLine($"Subtraction = {j - i}");
+            }
+        }
+        public void Multiply(int i, int j)
+        {
+            Console.WriteLine($"Multiplication = {j * i}");
+        }
+
+    }
+
+        // PS 4
+    public class Products
+    {
+        public int PId
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public void Display(Products p)
+        {
+            Console.WriteLine($"Product ID = {p.PId} and Product name is {p.Name}");
+        }
+    }
+        // PS 5
+    public class Employee
+    {
+        private DateTime joiningYear;
+        public DateTime Joining
+        {
+            get { return joiningYear; }
+            set { joiningYear = value; }
+        }
+        public int Expyrs(DateTime date)
+        {
+            int YearsOfService = DateTime.Now.Year - date.Year;
+            return YearsOfService;
+        }
+    }
+}
---
+++ Sept07/Program.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+
+namespace Sep07
+{
+    internal class Program
+    {
+        public delegate string stren5(string spo, string spo1);
+        public delegate string stren6(string hon);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PS 01");
+            Console.WriteLine("Enter the string");
+            string m = Console.ReadLine();
+            strin s1 = new strin();
+            stren s2 = new stren(s1.stringlen);
+            s2(m);
+            Console.WriteLine("==================");
+            Console.WriteLine("PS 02");
+            Console.WriteLine("Enter Username");
+            string user = Console.ReadLine();
+            stren s3 = new stren(s1.greet);
+            s3(user);
+            Console.WriteLine("==================");
+            Console.WriteLine("PS 03");
+            Console.WriteLine("Enter the two numbers");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            integer multicast = new integer();
+            stren2[] d3 = new stren2[3] { multicast.Add, multicast.Sub, multicast.Multiply };
+            stren2 d4 = (stren2)MulticastDelegate.Combine(d3);
+            d4(a, b);
+            Console.WriteLine("==================");
+            Console.WriteLine("PS 04");
+            Console.WriteLine("Enter Product ID and Product's name");
+            Products prod = new Products();
+            prod.PId = Convert.ToInt32(Console.ReadLine());
+            prod.Name = Console.ReadLine();
+            stren3 poi = new stren3(prod.Display);
+            poi(prod);
+            Console.WriteLine("==================");
+            Console.WriteLine("PS 05");
+            Console.WriteLine("Enter the Year,Month,Date in numeral format");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int month = Convert.ToInt32(Console.ReadLine());
+            int date = Convert.ToInt32(Console.ReadLine());
+            Employee emp = new Employee();
+            DateTime t = new DateTime(year, month, date);
+            emp.Joining = t;
+            stren4 se = new stren4(emp.Expyrs);
+            int yexp = se(emp.Joining);
+            Console.WriteLine($"Years of experience = {yexp}");
+            Console.WriteLine("==================");
+            Console.WriteLine("PS 06");
+            Console.WriteLine("Enter strings to Concatenate");
+            string text1 = Console.ReadLine();
+            string text2 = Console.ReadLine();
+            string str = text1+ text2;
+            stren5 text = delegate (string so, string su)
+            {
+                string str1 = so + su;
+                return str1;
+            };
+            string resultString = text(text1, text2);
+            Console.WriteLine($"Concatenated string is {resultString}");
+            Console.WriteLine("==================");
+            Console.WriteLine("PS 07");
+            // Console.WriteLine("Enter the string");
+            // string kon = Console.ReadLine();
+            stren6 df = delegate (string ki)
+            {
+                string p = " ";
+                for (int i = 0; i < ki.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        p += char.ToUpper(ki[i]);
+                    }
+                    else
+                    {
+                        p += ki[i];
+                    }
+                }
+
+                return p;
+            };
+            string pon = df(str);
+            Console.WriteLine($"First Case upper => {pon}");
+            Console.ReadLine();
+        }
+    }
+}
