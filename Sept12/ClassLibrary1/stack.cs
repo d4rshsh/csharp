e0d3636bbb7ee672255b63f5cfaf971fafa77d7d
darshshsh <ds14@gmail.com>
2022-09-13 15:44:16 +0530

    Sept12

---
+++ Sept12/ClassLibrary1/stack.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [Stereodisc("Song name", "language", "lyrics")]
+    public class song
+    {
+        [Stereodisc("Ae Dil Hai Mushkil", "Hindi", "lyrics")]
+        public string sName
+        {
+            get;
+            set;
+        }
+        [Stereodisc("Nee Kavithaigala", "Tamil", "lyrics")]
+        public int Count
+        {
+            get;
+        }
+
+
+    }
+}
---
+++ Sept12/Sept12/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Reflection;
+namespace Sept12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\sures\source\repos\Sept12\ClassLibrary1\bin\Debug\ClassLibrary.dll";
+            Assembly asm = Assembly.LoadFrom(path);
+            Type[] types = asm.GetTypes();
+            Type t = null;
+            foreach (var item in types)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.FullName);
+                MemberInfo[] min = item.GetMembers();
+                foreach (var item1 in min)
+                {
+                    Console.WriteLine(item1.Name);
+                }
+                MethodInfo[] max = item.GetMethods();
+                foreach (var item2 in max)
+                {
+                    Console.WriteLine(item2.Name);
+
+                    ParameterInfo[] pinfo = item2.GetParameters();
+                    foreach (var p in pinfo)
+                    {
+                        Console.WriteLine("Parameter Name: " + p.Name);
+                        Console.WriteLine("Parameter Position: " + p.Position);
+                        Console.WriteLine("Parameter Type: " + p.ParameterType);
+                    }
+                }
+                t = asm.GetType(item.FullName);
+                object obj = Activator.CreateInstance(t);
+
+
+
+                double cube = (double)item.InvokeMember("Cube1", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[1] { 400.0d });
+                Console.WriteLine("Cube number is" + " " + cube);
+                double sqrt = (double)item.InvokeMember("squareroot", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[1] { 200.0d });
+                Console.WriteLine("Square root number is" + " " + sqrt);
+                break;
+            }
+            Console.WriteLine("---------------");
+            Console.WriteLine("Custom attributes");
+            Program.WritedataForEachCode(typeof(song));
+            Console.Read();
+            Console.ReadLine();
+
+
+        }
+        static void WritedataForEachCode(Type t)
+        {
+            Console.WriteLine("Methods " + t.Name);
+
+            System.Attribute[] attrs = System.Attribute.GetCustomAttributes(t);  // Reflection.  
+
+            // Displaying output.  
+            foreach (System.Attribute attr in attrs)
+            {
+                if (attr is Stereodisc)
+                {
+                    Stereodisc a = (Stereodisc)attr;
+                    System.Console.WriteLine($"{a.songname} {a.slanguage} {a.slyrics}");
+                }
+            }
+            Console.WriteLine("---------------------------------");
+            MemberInfo[] minfo = t.GetMembers();
+
+            foreach (var item in minfo)
+            {
+                System.Attribute[] attrs1 = System.Attribute.GetCustomAttributes(item);
+
+                foreach (System.Attribute attrItem in attrs1)
+                {
+                    if (attrItem is Stereodisc)
+                    {
+                        Stereodisc a = (Stereodisc)attrItem;
+                        System.Console.WriteLine($"{a.songname} {a.slanguage} {a.slyrics}");
+                    }
+                }
+
+            }
+        }
+    }
+}
